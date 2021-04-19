8cb80999f1a36c1f1da37f11d853f584c4d0a581
Anwesh43 <anweshthecool0@gmail.com>
2021-04-20 03:16:53 +0530

    adding SquareCreateDashView class

--- squarecreatedashview/src/main/java/com/example/squarecreatedashview/SquareCreateDashView.kt
+++ squarecreatedashview/src/main/java/com/example/squarecreatedashview/SquareCreateDashView.kt
@@ -76,3 +76,19 @@ fun Canvas.drawSCDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareCreateDash(scale, w, h, paint)
 }
+
+class SquareCreateDashView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
