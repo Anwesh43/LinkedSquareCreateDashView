bea5b13213be906c191806ee6d6a000a88e7965e
Anwesh43 <anweshthecool0@gmail.com>
2021-04-20 03:15:25 +0530

    adding Canvas extension methods

--- squarecreatedashview/src/main/java/com/example/squarecreatedashview/SquareCreateDashView.kt
+++ squarecreatedashview/src/main/java/com/example/squarecreatedashview/SquareCreateDashView.kt
@@ -25,8 +25,54 @@ val sizeFactor : Float = 3.9f
 val barFactor : Float = 6.6f
 val rot : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val lines : Int = 4
+val parts : Int = 1 + lines
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSquareCreateDash(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val size : Float = Math.min(w, h) / sizeFactor
+    val barSize : Float = Math.min(w, h) / barFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate(rot * j)
+        drawLine(
+            size,
+            -size,
+            size,
+            -size + 2 * size * sf.divideScale(j, parts),
+            paint
+        )
+        restore()
+    }
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawRect(
+            RectF(
+                -size,
+                -size,
+                -size + barSize * sf.divideScale(lines, parts),
+                size
+            ),
+            paint
+        )
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSCDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareCreateDash(scale, w, h, paint)
+}
