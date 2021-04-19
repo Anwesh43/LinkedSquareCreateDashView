2869fde7eab6346163f7ae0c13fd413b0f9a2163
Anwesh43 <anweshthecool0@gmail.com>
2021-04-20 03:07:34 +0530

    adding constants and libraries

--- squarecreatedashview/src/main/java/com/example/squarecreatedashview/SquareCreateDashView.kt
+++ squarecreatedashview/src/main/java/com/example/squarecreatedashview/SquareCreateDashView.kt
@@ -1,2 +1,27 @@
 package com.example.squarecreatedashview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#4CAF50",
+    "#00C853",
+    "#1A237E"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val scGap : Float = 0.02f
+val delay : Long = 20
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.9f
+val barFactor : Float = 6.6f
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
